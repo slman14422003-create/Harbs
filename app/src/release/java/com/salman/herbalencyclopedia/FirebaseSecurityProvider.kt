ca5ea109bd048c133fdde6567c7552f36555ab9b
slx23m <slman14422003@gmail.com>
2026-08-31 15:58:58 +0300

    Add files via upload

--- app/src/release/java/com/salman/herbalencyclopedia/FirebaseSecurityProvider.kt
+++ app/src/release/java/com/salman/herbalencyclopedia/FirebaseSecurityProvider.kt
@@ -1,1 +1,10 @@
+package com.salman.herbalencyclopedia
 
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+
+internal fun installAppCheckProvider() {
+    FirebaseAppCheck.getInstance().installAppCheckProviderFactory(
+        PlayIntegrityAppCheckProviderFactory.getInstance()
+    )
+}
